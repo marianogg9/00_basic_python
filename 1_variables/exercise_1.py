c0d8716f7db4ba98015bf9467357dc31929e2290
aldazar <alexandreblanchet44@gmail.com>
2023-03-25 11:39:20 +0000

    update exercises

---
+++ 1_variables/exercise_1.py
@@ -0,0 +1,5 @@
+# TODO: Create a variable called message containing the value "Hello World"
+
+# Your code here
+
+assert message == "Hello World"
---
+++ 1_variables/exercise_2.py
@@ -0,0 +1,9 @@
+# TODO: Swap values between two variables
+
+first_word = "Car"
+second_word = "Bike"
+
+# Your code here
+
+assert first_word == "Bike"
+assert second_word == "Car"
